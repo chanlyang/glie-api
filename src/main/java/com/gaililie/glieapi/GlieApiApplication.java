8305c417e4c7557e82305066f400a2bebb57712f
chenliang <chenliang@shuidihuzhu.com>
2019-11-17 19:00:18 +0800

    Initial commit

---
+++ src/main/java/com/gaililie/glieapi/GlieApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaililie.glieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaililie/glieapi/GlieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaililie.glieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
